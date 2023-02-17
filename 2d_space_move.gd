5e5f70494efd12ab537d8cd223204014941da99d
Poyram <78064653+Poyram@users.noreply.github.com>
2023-02-17 21:00:37 +0300

    Update 2d_space_move.gd

--- 2d_space_move.gd
+++ 2d_space_move.gd
@@ -2,13 +2,15 @@ extends CharacterBody2D
 
 const SPEED = 60.0
 const MAX_SPEED = 400.0
+const SLOW_SPEED = 5.0
+var direction = Vector2(0, 0)
 
 func _physics_process(_delta):
 	# mouse angel
 	var mouse_angel = Vector2.LEFT.angle_to(get_viewport().get_mouse_position() - Vector2(get_window().get_size())/2)
 	direction = Vector2(Input.get_axis("ui_left", "ui_right"),Input.get_axis("ui_up", "ui_down")).normalized()
 	if direction:
-		velocity += direction * SPEED_OLD * _delta * 60
+		velocity += direction * SPEED * _delta * 60
 		if velocity.length() > MAX_SPEED:
 			velocity = velocity.normalized() * MAX_SPEED
 	else:
